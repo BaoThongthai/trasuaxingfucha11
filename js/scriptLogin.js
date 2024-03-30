74a82a09afb931d0b47adfa096088186d30bc9c8
unknown <hoanglephibao.1212@gmail.com>
2024-03-30 20:45:05 +0700

    updategink

--- js/scriptLogin.js
+++ js/scriptLogin.js
@@ -21,7 +21,7 @@ document.getElementById("loginButton").addEventListener("click", function () {
     const formMessage_error = document.getElementById("formMessage_error");
     if (enteredUsername.value === userData.fullName && enteredPassword.value === userData.password) {
         alert("Đăng nhập thành công")
-        window.location = "Project_xingfucha.html";
+        window.location = "index.html";
     } else {
         formMessage_error.textContent = ' **Sai Thông Tin'
     }

467c14e716973207476a02a47dbd1352f26f1768
Baothongthai <hoanglephibao.1212@gmail.com>
2025-02-11 19:08:45 +0700

    infor store

---
+++ inforstore.js
@@ -0,0 +1,20 @@
+function spark(event) {
+    let i = document.createElement('i');
+    i.style.left = (event.pageX) + 'px';
+    i.style.top = (event.pageY) + 'px';
+    i.style.scale = `${Math.random() * 2 + 1}`;
+    i.style.setProperty('--x', getRandomTransitionValue());
+    i.style.setProperty('--y', getRandomTransitionValue());
+
+    document.body.appendChild(i);
+
+    setTimeout(() => {
+        document.body.removeChild(i);
+    }, 2000)
+}
+
+function getRandomTransitionValue() {
+    return `${Math.random() * 400 - 200}px`;
+}
+
+document.addEventListener('mousemove', spark);
